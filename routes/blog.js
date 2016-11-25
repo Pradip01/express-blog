5d48f9e608466983ace454c17a88b779b324b7ac
Pradip01 <pradip.kolhe@raweng.com>
2016-11-25 10:06:37 +0530

    Add files via upload
    commit

--- routes/blog.js
+++ routes/blog.js
@@ -1,29 +1,28 @@
 var express = require('express');
-var nunjucks  = require('nunjucks');
+var nunjucks = require('nunjucks');
 var router = express.Router();
 var Contentstack = require('contentstack');
 
 /* GET home page. */
 router.get('/:id', function (req, res) {
-    try{
+    try {
 
-        var Stack = Contentstack.Stack('bltef7ae7ae7535633b', 'blt681875b8942ecfbc','development');
-
+        var Stack = req.app.locals.Stack;
         var Query = Stack.ContentType('blog_post').Entry(req.params.id);
 
         Query
-      .fetch()
-   .then(function success(entry) {
-            var myEntry = entry.toJSON();
-            var data ={data: myEntry}
-            res.render('blog.html', data);
+            .fetch()
+            .then(function success(entry) {
+                var myEntry = entry.toJSON();
+                var data = {data: myEntry}
+                res.render('blog.html', data);
 
-        }, function error(err) {
-            res.send(err)
-        });
+            }, function error(err) {
+                res.send(err)
+            });
 
 
-    }catch (e){
+    } catch (e) {
         console.log("Error ", e)
     }
 
